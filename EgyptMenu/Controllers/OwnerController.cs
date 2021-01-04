bfa8d1d99b6d6e4f6c1662b724cd02e3261dae7a
AhmedElgbaly30 <ahmedelgbaly30@gmail.com>
2021-01-04 11:48:51 +0200

    kkkk

---
+++ EgyptMenu/Controllers/OwnerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EgyptMenu.Controllers
+{
+    //[Authorize(Roles ="Owner")]
+    public class OwnerController : Controller
+    {
+        // GET: Owner
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        public ActionResult Menu()
+        {
+            return View();
+        }
+        public ActionResult QRBuilder()
+        {
+            return View();
+        }
+        public ActionResult Plan()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public new ActionResult Profile()
+        {
+            return View();
+        }
+        public ActionResult Restaurant()
+        {
+            return View();
+        }
+
+    }
+}
